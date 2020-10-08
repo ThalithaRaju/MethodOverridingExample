b51eb3b4aaa9ae852cd24be1f6499ddf5746ffdd
ThalithaRaju <thalitharaju22@gmail.com>
2020-10-08 22:18:24 +0530

    first commit

---
+++ MethodOverridingExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverridingExample
+{
+    class BaseClass
+        {
+        public  virtual void  Show()
+        {
+            Console.WriteLine("Base class");
+        }
+
+        }
+    class Derived:BaseClass
+    {
+        public override  void Show()
+        {
+            Console.WriteLine("Derived class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass obj;
+            obj = new BaseClass();
+            obj.Show();
+            obj = new Derived();
+            obj.Show();
+            //BaseClass obj = new BaseClass();
+            //obj.Show();
+            //BaseClass obj = new Derived();
+            Console.ReadKey();
+        }
+    }
+}
